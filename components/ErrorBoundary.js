2f678ba89e53f80b43e9a3c53bfefb4a5057960a
Godwin254 <godwinbee15@gmail.com>
2020-07-01 10:57:26 -0400

    method to render fallback UI and to log error messages

--- components/ErrorBoundary.js
+++ components/ErrorBoundary.js
@@ -6,9 +6,23 @@ class ErrorBoundary extends Component{
     this.state= { hasError: false}
   }
   
+  static getDerivedStateFromError(error){
+    return {hasError: true}
+  }
+
+  componentDidCatch(error, errorInfo){
+
+    logErrorToMyService(error, errorInfo);
+
+  }
 
   render(){
-    
+    if(this.state.hasError){
+      return (
+        <h3>Something went wrong..</h3>
+      )
+    }
+
     return this.props.children
   }
 
