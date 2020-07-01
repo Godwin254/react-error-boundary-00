cd9f38c5ebbde3897231b3360a5c16f26d57b7d6
Godwin254 <godwinbee15@gmail.com>
2020-07-01 11:14:11 -0400

    catch errors on event handlers

--- components/TestComponent.js
+++ components/TestComponent.js
@@ -5,6 +5,18 @@ function TestComponent() {
   let initialState = 0
   const [count, setCount] = useState(initialState)
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    //use try/catch to log errors on event handlers
+    try{
+      setCount( count + 1 );
+    }
+    catch(error){
+      console.log(error);
+    }
+    
+  }
+
   return (
     <div>
 
